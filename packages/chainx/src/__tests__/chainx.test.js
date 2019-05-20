17d91d8a085d870905e1a819640a6aaf4609c521
yznt <nsso@foxmail.com>
2019-05-20 16:52:01 +0800

    test file

--- packages/chainx/src/__tests__/chainx.test.js
+++ packages/chainx/src/__tests__/chainx.test.js
@@ -9,18 +9,21 @@ describe('chainx.js', () => {
   });
 
   it('test', done => {
-    const a = chainx.asset.claimSdot(
-      {
-        r: '0x14e496e4182afe7fd6e248f996adf2810cda35ef4577be8ed8b10028f2a4b7ec',
-        s: '0x0ad92ad5c6fc126c08d4dff9cab7fc4df5f4940b4b823dbb56b2acd2919e173e',
-        v: 1,
-      },
-      '0xf85681a385012a05f200830f424094eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee80b0355074646d6a7764374b4c5a533344596a5239524739367a577352654d6235463448574a7434334e61484c53526e6542018080',
-      '0x355074646d6a7764374b4c5a533344596a5239524739367a577352654d6235463448574a7434334e61484c53526e6542'
-    );
+    // const a = chainx.asset.claimSdot(
+    //   {
+    //     r: '0x14e496e4182afe7fd6e248f996adf2810cda35ef4577be8ed8b10028f2a4b7ec',
+    //     s: '0x0ad92ad5c6fc126c08d4dff9cab7fc4df5f4940b4b823dbb56b2acd2919e173e',
+    //     v: 1,
+    //   },
+    //   '0xf85681a385012a05f200830f424094eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee80b0355074646d6a7764374b4c5a533344596a5239524739367a577352654d6235463448574a7434334e61484c53526e6542018080',
+    //   '0x355074646d6a7764374b4c5a533344596a5239524739367a577352654d6235463448574a7434334e61484c53526e6542'
+    // );
 
-    chainx.asset.getSdotClaims('0x007e917588d7a1392c3604501e00a73565d06845').then(data => {
-      console.log(data);
-    });
+    // chainx.asset.getSdotClaims('0x007e917588d7a1392c3604501e00a73565d06845').then(data => {
+    //   console.log(data);
+    // });
+
+    const e = chainx.asset.transfer(chainx.account.from('qweqwe').address(), 'BTC', 100000, 'eqweq');
+    console.log(e);
   });
 });
