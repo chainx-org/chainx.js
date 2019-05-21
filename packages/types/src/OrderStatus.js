81991209970038016080b37e860e425d00934f4e
yznt <nsso@foxmail.com>
2019-05-21 17:37:03 +0800

    update

--- packages/types/src/OrderStatus.js
+++ packages/types/src/OrderStatus.js
@@ -1,6 +1,6 @@
 import Enum from './codec/Enum';
 
-export default class OrderType extends Enum {
+export default class OrderStatus extends Enum {
   constructor(index) {
     super(['ZeroFill', 'ParitialFill', 'Filled', 'ParitialFillAndCanceled', 'Canceled'], index);
   }
