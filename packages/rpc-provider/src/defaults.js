c31bb29a3789f93f23508922398ecf5f0c0f6327
yznt <nsso@foxmail.com>
2019-05-28 11:01:15 +0800

    default provider

--- packages/rpc-provider/src/defaults.js
+++ packages/rpc-provider/src/defaults.js
@@ -2,9 +2,9 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-const HTTP_URL = 'http://47.93.16.189:8081';
+const HTTP_URL = 'http://127.0.0.1:8086';
 
-const WS_URL = 'ws://47.93.16.189:8082';
+const WS_URL = 'ws://127.0.0.1:8087';
 
 export default {
   HTTP_URL,
