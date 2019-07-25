9d36731b81cfb41a74eaee941049c0f92c70d78b
yznt <nsso@foxmail.com>
2019-07-25 10:46:01 +0800

    upadte

--- packages/chainx/src/__tests__/chainx.test.js
+++ packages/chainx/src/__tests__/chainx.test.js
@@ -20,28 +20,5 @@ describe('chainx.js', () => {
     console.error(multiSigAddrInfo);
     const pendingListFor = await chainx.trustee.getPendingListFor(accounts.trusteesAccount.Bitcoin);
     console.error(JSON.stringify(pendingListFor));
-    // const e = chainx.trustee.removeMultiSigFor(
-    //   '0xb106fb4dc17f3e7671a1aed77c5fdf0f83dbc47793a50bc2dd115587640e641e', // 地址
-    //   '0xe1b9ba50f7bc98a5094dcef1dcc1709155d3274c492b6638beeff78d29b12d9f' // proposal id
-    // );
-
-    // const e = chainx.trustee.execute(
-    //   '0xb106fb4dc17f3e7671a1aed77c5fdf0f83dbc47793a50bc2dd115587640e641e',
-    //   chainx.api.tx.xBridgeFeatures.transitionTrusteeSession('Bitcoin', [
-    //     '5FjN2LCaoRfCR6Z78XHBE6SMCUPFPRWQD7bHU5M8qxrhTxjj',
-    //     '5ELXHYwfbNmovjN1BwFZaGJBczAuTuBdzNBSU4bUWLz6cvW3',
-    //     '5E24tsypCtNAoHuC6UVfrKNkcHQmPzKRuLaNLgAWAig3KtCa',
-    //   ])
-    // );
-
-    // e.signAndSend('0x436861696e582d416c6963652020202020202020202020202020202020202020', (error, response) => {
-    //   if (error) {
-    //     console.log(error);
-    //   } else if (response.status === 'Finalized') {
-    //     if (response.result === 'ExtrinsicSuccess') {
-    //       console.log('交易成功');
-    //     }
-    //   }
-    // });
   });
 });
