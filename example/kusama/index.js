607a196c1869f0b5b84690dd6682f4f83e2e3483
yznt <nsso@foxmail.com>
2019-08-28 14:55:36 +0800

    update

--- example/kusama/index.js
+++
@@ -1,6 +0,0 @@
-// 用于不断提交Kusama转账交易，并更新数据库状态
-const { getApi } = require('./ksm');
-
-(async function() {
-  const ksmApi = await getApi();
-})();
--- example/kusama/ksm.js
+++
@@ -1,3 +0,0 @@
-const { ApiPromise, WsProvider } = require('@polkadot/api');
-const provider = new WsProvider('ws://140.82.35.183:9944');
-ApiPromise.create(provider);
