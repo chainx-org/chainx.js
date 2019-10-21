06e2f4a8bb1751b7615100980efe06404c6eb17b
nsso@foxmail.com <nsso@foxmail.com>
2019-10-21 10:14:48 +0800

    update

--- packages/types/src/Metadata/util/toCallsOnly.js
+++ packages/types/src/Metadata/util/toCallsOnly.js
@@ -1,12 +1,12 @@
 // Copyright 2017-2019 @polkadot/types authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
-import MetadataV8 from '../v8';
+import MetadataV5 from '../v5';
 /**
  * @description Convert from MetadataV8 to a stripped representation of MetadataV8
  */
 export default function toCallsOnly({ modules }) {
-  return new MetadataV8({
+  return new MetadataV5({
     // FIXME, this needs typing, not any
     modules: modules.map(({ calls, name }) => ({
       name,
