94c309eac98198a371e31c24c63af4214d92a2cf
nsso@foxmail.com <nsso@foxmail.com>
2019-11-12 17:31:02 +0800

    update

--- packages/types/src/ERC20Selector.js
+++ packages/types/src/ERC20Selector.js
@@ -2,6 +2,6 @@ import Enum from './codec/Enum';
 
 export default class ERC20Selector extends Enum {
   constructor(index) {
-    super(['Issue', 'BalanceOf', 'TotalSupply', 'Name', 'Symbol', 'Decimals'], index);
+    super(['BalanceOf', 'TotalSupply', 'Name', 'Symbol', 'Decimals', 'Issue', 'Destory'], index);
   }
 }
