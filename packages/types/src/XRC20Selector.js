11004ced77e649ad112084c77839dde9c472091c
nsso@foxmail.com <nsso@foxmail.com>
2019-11-13 17:09:31 +0800

    update

--- packages/types/src/XRC20Selector.js
+++ packages/types/src/XRC20Selector.js
@@ -4,4 +4,9 @@ export default class XRC20Selector extends Enum {
   constructor(index) {
     super(['BalanceOf', 'TotalSupply', 'Name', 'Symbol', 'Decimals', 'Issue', 'Destroy'], index);
   }
+
+  // 发送交易
+  toJSON() {
+    return Object.keys(this._def)[this._index];
+  }
 }
