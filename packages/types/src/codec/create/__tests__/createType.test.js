806ce49ccc620637d53f30fc2f77b55e624e8ef2
qinghuan <hashlock@163.com>
2020-03-15 17:46:18 +0800

    fix test

--- packages/types/src/codec/create/__tests__/createType.test.js
+++
@@ -1,9 +0,0 @@
-import '../../../injector';
-
-import { createType } from '..';
-import staticdata from '../static';
-
-describe('createType', () => {
-  // @ts-ignore
-  console.log(createType('Metadata', [staticdata]));
-});
